5fc65f556d2de7ac23f125c9c4373fab45dce24e
Aleja Ocampo <omaira.527@hotmail.com>
2020-11-26 14:50:19 -0500

    adding files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+console.log("Hola mundo desde parcel");
+
+
+import '../src/styles/base.scss';
+import fullpage from './fullpage_js/fullpage';
+
+const sectionPage = new fullpage('#fullpage', {
+  licenseKey: 'YOUR_KEY_HERE'
+})  
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      rale:['Roboto'],
+      conden:['Roboto Condensed']
+
+    },
+    extend: {
+      colors:{
+        danger:'#E6739F'
+      },
+      translate:{
+        '1/7':'30%'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
