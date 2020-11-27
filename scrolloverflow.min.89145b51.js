bf7de97bb825d280a86e54d4d5171ae6771ee45b
Aleja Ocampo <omaira.527@hotmail.com>
2020-11-26 20:48:32 -0500

    Updates

--- scrolloverflow.min.89145b51.js
+++ scrolloverflow.min.89145b51.js
@@ -1149,7 +1149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49492" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60967" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
